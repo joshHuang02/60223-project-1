db173cf7c5ce1e81c889526340085d89712da498
joshHuang02 <52260865+joshHuang02@users.noreply.github.com>
2023-09-12 16:55:56 -0400

    added solenoid stuff beginning

--- project-1-code/project-1-code.ino
+++ project-1-code/project-1-code.ino
@@ -1,15 +1,26 @@
 #define backLight 13
-#define photoRes A0
+#define solenoidLow 8
+#define solenoidHigh 9
+#define photoRes A0 //0-1023, actual 750-1000
+
+int photoVal = 0;
 
 void setup() {
   pinMode(backLight, OUTPUT);
   pinMode(photoRes, INPUT);
+  pinMode(solenoidLow, OUTPUT);
 
   Serial.begin(9600);
 }
 
 void loop() {
-  Serial.println(analogRead(photoRes));
+  photoVal = analogRead(photoRes);
   delay(10);
+  if (photoVal > 930) {
+    digitalWrite(solenoidLow, HIGH);
+    Serial.println(photoVal);
+  } else {
+    digitalWrite(solenoidLow, LOW);
+  }
 
 }
